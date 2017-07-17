abf4d6fc8837113e4e751b9f837bdba2b5ea84f0
Rahul Mishra <rahul.mishra.ec054@gmail.com>
2017-07-17 22:58:07 +0530

    no message

---
+++ LineZen/Assets/Script/Utils/TimeConversion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public static  class TimeConversion 
+{
+
+	public static double DateTimeToUnixTimestamp(DateTime dateTime)
+	{
+		return (TimeZoneInfo.ConvertTimeToUtc(dateTime) - 
+			new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc)).TotalSeconds;
+	}
+
+	public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+	{
+		DateTime dtDateTime = new DateTime (1970, 1, 1 ,0, 0, 0, 0,DateTimeKind.Utc);
+		dtDateTime = dtDateTime.AddSeconds (unixTimeStamp).ToLocalTime();
+		return dtDateTime;
+	}
+
+}
